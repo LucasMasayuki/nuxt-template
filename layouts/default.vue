0a957450290f6d74fb6c19c12c6405d51987e0fb
lucas <lutamaribuchi@gmail.com>
2023-01-12 02:21:09 -0300

    feat: init template

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import AppBar from '@/components/app/AppBar.vue'
+import AppFooter from '@/components/app/AppFooter.vue'
+</script>
+
+<template>
+    <div>
+        <v-navigation-drawer></v-navigation-drawer>
+
+        <AppBar />
+
+        <v-main>
+            <slot></slot>
+        </v-main>
+        <AppFooter />
+    </div>
+</template>
